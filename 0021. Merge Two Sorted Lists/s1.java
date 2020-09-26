60e80cbc930a23847e1a550c9fc693c74862d930
ptsang-hcmus <ptsang.hcmus@gmail.com>
2020-09-27 01:17:52 +0700

    21. Merge Two Sorted Lists

---
+++ 0021. Merge Two Sorted Lists/s1.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode x = new ListNode();
+        ListNode dummy = x;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                dummy.next = l1;
+                l1 = l1.next;
+            } else {
+                dummy.next = l2;
+                l2 = l2.next;
+            }
+            dummy = dummy.next;
+        }
+        dummy.next = l1 != null ? l1 : l2;
+        return x.next;
+    }
+}
